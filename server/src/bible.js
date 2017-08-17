009cfb713646755bd0b93d9c8cfc5a993140c1e4
Tris Setiawan <tris.setiawan@yahoo.com>
2017-08-17 07:43:50 +0000

    initial

--- server/src/bible.js
+++ server/src/bible.js
@@ -8,6 +8,7 @@ for (var book of oldTestaments)
 for (var book of newTestaments)
     bookMap.set(book, "N");
 
+const action = /^bible-read/i;
 
 class Bible {
     constructor() {
@@ -16,12 +17,11 @@ class Bible {
     middleware() {
         return (req, res, next) => {
             var payload = req.body;
-            if (payload.result.action !== "bible.read" || payload.result.actionIncomplete)
+            if (!payload.result.action.match(action) || payload.result.actionIncomplete)
                 next();
             else {
                 this.load(payload)
                     .then(result => {
-                        var response = result;
                         res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
                         res.send(JSON.stringify(result));
                     })
@@ -36,27 +36,26 @@ class Bible {
             }
         };
     }
-
-
     load(data) {
-        var parameters = data.result.parameters;
-        var bible = parameters.bible || {};
-        var version = bible.version || parameters.version;
-        var book = bible.book || parameters.book;
-        var chapter = bible.chapter || parameters.chapter;
-        var verseStart = Math.abs(parseInt(bible["verse-start"] || parameters["verse-start"] || 1, 10));
-        var verseEnd = Math.abs(parseInt(bible["verse-end"] || parameters["verse-end"] || verseStart || 1, 10));
+        var parameter = this.getParameter(data);
+        var action = data.result.action;
 
-        var totalVerses = verseEnd - (verseStart - 1);
-        var page = parseInt(parameters.page || 1);
-        var size = parseInt(parameters.size || 5);
+        if (action === "bible-read-more")
+            parameter.page += 1;
 
-        var from = page === 1 ? verseStart : (page - 1) * size;
-        var to = verseEnd > (from + size - 1) ? (from + size - 1) : verseEnd;
+        var damId = `${parameter.version}${bookMap.get(parameter.book)}2ET`;
+        var from = 1 + (parameter.page - 1) * parameter.size;
+        var to = parameter.page * parameter.size;
 
-        var damId = `${version}${bookMap.get(book)}2ET`;
-
-        var uri = `http://dbt.io/text/verse?key=${process.env.DBT_KEY}&dam_id=${damId}&book_id=${book}&chapter_id=${chapter}&verse_start=${from}&verse_end=${to}&v=2`;
+        var uri = `http://dbt.io/text/verse?key=${process.env.DBT_KEY}&dam_id=${damId}&book_id=${parameter.book}&chapter_id=${parameter.chapter}&verse_start=${from}&verse_end=${to}&v=2`;
+ 
+        var outMore = {
+            "name": "bible-read-more",
+            "lifespan": 5,
+            "parameters": parameter
+        };
+        var outVersion = Object.assign({}, outMore);
+        outVersion.name = "bible-read-version";
 
         return fetch(uri)
             .then(result => result.json())
@@ -65,23 +64,36 @@ class Bible {
                 return {
                     "speech": response,
                     "displayText": response,
-                    "contextOut": [{
-                        "name": "bible-reading-followup ",
-                        "lifespan": 1,
-                        "parameters": {
-                            "version": version,
-                            "book": version,
-                            "chapter": version,
-                            "verse-start": version,
-                            "verse-end": version,
-                            "page": page+1,
-                            "size": size
-
-
-                        }
-                    }]
+                    "data": {},
+                    "source": "bible-webhook",
+                    "contextOut": [outMore, outVersion]
                 };
             });
+    }
+
+    getParameter(data) {
+        var parameters = data.result.parameters;
+        var version = parameters.version;
+        var book = parameters.book;
+        var chapter = parameters.chapter;
+        var from = Math.abs(parseInt(parameters.from || 1, 10));
+        var to = Math.abs(parseInt(parameters.to || from || 1, 10));
+
+        var page = parseInt(parameters.page || 1, 10);
+        
+        var size = from === to ? 1 : parseInt(parameters.size || 5, 10);
+        var sizeChange = parseInt(parameters["size-change"] || size, 10);
+        size = sizeChange === size ? size : sizeChange;
+        
+        return {
+            "version": version,
+            "book": book,
+            "chapter": chapter,
+            "from": from,
+            "to": to,
+            "page": page,
+            "size": size
+        };
     }
 }
 
