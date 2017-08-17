4e2fba03b2a28ccdc6859a9c07c51f0f38a71498
Tris Setiawan <tris.setiawan@yahoo.com>
2017-08-17 08:26:45 +0000

    initial

--- server/src/bible.js
+++ server/src/bible.js
@@ -1,13 +1,5 @@
 const fetch = require("node-fetch");
-
-const oldTestaments = ["Gen", "Exod", "Lev", "Num", "Deut", "Josh", "Judg", "Ruth", "1Sam", "2Sam", "1Kgs", "2Kgs", "1Chr", "2Chr", "Ezra", "Neh", "Esth", "Job", "Ps", "Prov", "Eccl", "Song", "Isa", "Jer", "Lam", "Ezek", "Dan", "Hos", "Joel", "Amos", "Obad", "Jonah", "Mic", "Nah", "Hab", "Zeph", "Hag", "Zech", "Mal"];
-const newTestaments = ["Matt", "Mark", "Luke", "John", "Acts", "Rom", "1Cor", "2Cor", "Gal", "Eph", "Phil", "Col", "1Thess", "2Thess", "1Tim", "2Tim", "Titus", "Phlm", "Heb", "Jas", "1Pet", "2Pet", "1John", "2John", "3John", "Jude", "Rev"];
-const bookMap = new Map();
-for (var book of oldTestaments)
-    bookMap.set(book, "O");
-for (var book of newTestaments)
-    bookMap.set(book, "N");
-
+const volumes = require("./bible-volume");
 const action = /^bible-read/i;
 
 class Bible {
@@ -43,12 +35,12 @@ class Bible {
         if (action === "bible-read-more")
             parameter.page += 1;
 
-        var damId = `${parameter.version}${bookMap.get(parameter.book)}2ET`;
+        var damId = `${parameter.version}${volumes.get(parameter.book).volume}2ET`;
         var from = 1 + (parameter.page - 1) * parameter.size;
         var to = parameter.page * parameter.size;
 
         var uri = `http://dbt.io/text/verse?key=${process.env.DBT_KEY}&dam_id=${damId}&book_id=${parameter.book}&chapter_id=${parameter.chapter}&verse_start=${from}&verse_end=${to}&v=2`;
- 
+
         var outMore = {
             "name": "bible-read-more",
             "lifespan": 5,
@@ -80,11 +72,11 @@ class Bible {
         var to = Math.abs(parseInt(parameters.to || from || 1, 10));
 
         var page = parseInt(parameters.page || 1, 10);
-        
+
         var size = from === to ? 1 : parseInt(parameters.size || 5, 10);
         var sizeChange = parseInt(parameters["size-change"] || size, 10);
         size = sizeChange === size ? size : sizeChange;
-        
+
         return {
             "version": version,
             "book": book,
