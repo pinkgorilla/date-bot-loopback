2864fd889d63a84ec3700e2f42ecca9371e7aa17
Tris Setiawan <tris.setiawan@yahoo.com>
2017-08-18 04:46:05 +0000

    fix bible reading

--- server/src/bible.js
+++ server/src/bible.js
@@ -35,31 +35,38 @@ class Bible {
         if (action === "bible-read-more")
             parameter.page += 1;
 
-        var damId = `${parameter.version}${volumes.get(parameter.book).volume}2ET`;
-        var from = 1 + (parameter.page - 1) * parameter.size;
-        var to = parameter.page * parameter.size;
+        return this.getVerses(parameter)
+            .then(verses => {
+                var outMore = {
+                    "name": "bible-read-more",
+                    "lifespan": 5,
+                    "parameters": parameter
+                };
+                var outVersion = Object.assign({}, outMore);
+                outVersion.name = "bible-read-version";
 
-        var uri = `http://dbt.io/text/verse?key=${process.env.DBT_KEY}&dam_id=${damId}&book_id=${parameter.book}&chapter_id=${parameter.chapter}&verse_start=${from}&verse_end=${to}&v=2`;
-
-        var outMore = {
-            "name": "bible-read-more",
-            "lifespan": 5,
-            "parameters": parameter
-        };
-        var outVersion = Object.assign({}, outMore);
-        outVersion.name = "bible-read-version";
-
-        return fetch(uri)
-            .then(result => result.json())
-            .then(json => {
-                var response = json.map(verse => `${verse.verse_id}. ${verse.verse_text}`).join("\n");
                 return {
-                    "speech": response,
-                    "displayText": response,
+                    "speech": verses,
+                    "displayText": verses,
                     "data": {},
                     "source": "bible-webhook",
                     "contextOut": [outMore, outVersion]
                 };
+            });
+    }
+
+    getVerses(parameter) {
+        var damId = `${parameter.version}${volumes.get(parameter.book).volume}2ET`;
+        var totalVerse = (parameter.to - parameter.from) + 1;
+
+        var from = totalVerse <= parameter.size ? parameter.from + (parameter.page - 1) * totalVerse : parameter.from + (parameter.page - 1) * parameter.size;
+        var to = totalVerse <= parameter.size ? (from + totalVerse - 1) : parameter.page * parameter.size;
+
+        var uri = `http://dbt.io/text/verse?key=${process.env.DBT_KEY}&dam_id=${damId}&book_id=${parameter.book}&chapter_id=${parameter.chapter}&verse_start=${from}&verse_end=${to}&v=2`;
+        return fetch(uri)
+            .then(result => result.json())
+            .then(json => {
+                return json.map(verse => `${verse.verse_id}. ${verse.verse_text}`).join("\n");
             });
     }
 
@@ -68,12 +75,14 @@ class Bible {
         var version = parameters.version;
         var book = parameters.book;
         var chapter = parameters.chapter;
+
         var from = Math.abs(parseInt(parameters.from || 1, 10));
         var to = Math.abs(parseInt(parameters.to || from || 1, 10));
+        var arr = [from, to].sort((a, b) => a - b);
 
         var page = parseInt(parameters.page || 1, 10);
 
-        var size = from === to ? 1 : parseInt(parameters.size || 5, 10);
+        var size = parseInt(parameters.size || 5, 10);
         var sizeChange = parseInt(parameters["size-change"] || size, 10);
         size = sizeChange === size ? size : sizeChange;
 
@@ -81,8 +90,8 @@ class Bible {
             "version": version,
             "book": book,
             "chapter": chapter,
-            "from": from,
-            "to": to,
+            "from": arr[0],
+            "to": arr[1],
             "page": page,
             "size": size
         };
